746e2a640127eb873bc1228d9029818bc14a6042
Bedada <45728023+Bedadha@users.noreply.github.com>
2021-09-14 19:42:15 +0530

    Update settings.py

--- lsap/lsap/settings.py
+++ lsap/lsap/settings.py
@@ -127,7 +127,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR,'lsap/static/')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-
+django_heroku.settings(locals())
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
